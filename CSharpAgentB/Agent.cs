0ab1449ac967c28e82f0b88799f75a9f2d375364
Peter Charij <CHARIJPX@legal.regn.net>
2022-02-10 14:09:31 +0000

    Fane & Vu

--- CSharpAgentB/Agent.cs
+++ CSharpAgentB/Agent.cs
@@ -7,32 +7,85 @@ using PlanetWars.Shared;
 
 namespace CSharpAgent
 {
+    public class planetCost
+    {
+        public planetCost(Planet planet, int cost)
+        {
+            this.planet = planet;
+            this.cost = cost;
+        }
+        public Planet planet { get; set; }
+        public int cost { get; set; }
+
+    }
+
     public class Agent : AgentBase
     {
-        public Agent(string name, string endpoint) : base(name, endpoint){}
+        public Agent(string name, string endpoint) : base(name, endpoint) { }
 
         public override void Update(StatusResult gs)
         {
             // do cool ai stuff
             Console.WriteLine($"[{DateTime.Now.ToShortTimeString()}]Current Turn: {gs.CurrentTurn}");
             Console.WriteLine($"My ID: {MyId}");
-            Console.WriteLine($"Owned Planets: {string.Join(", ", gs.Planets.Where(p => p.OwnerId == MyId).Select(p =>  p.Id))}");
+            Console.WriteLine($"Owned Planets: {string.Join(", ", gs.Planets.Where(p => p.OwnerId == MyId).Select(p => p.Id))}");
+            var opplayer = 0;
+            int ownedShips = 0;
+            if (gs.PlayerA == MyId)
+            {
+                opplayer = gs.PlayerB;
+            }
+            else
+            {
+                opplayer = gs.PlayerA;
+            };
 
             // find the first planet we don't own
             var targetPlanet = gs.Planets.FirstOrDefault(p => p.OwnerId != MyId);
             if (targetPlanet == null) return; // WE OWN IT ALLLLLLLLL
 
-            Console.WriteLine($"Target Planet: {targetPlanet.Id}:{targetPlanet.NumberOfShips}");                       
+            var targetList = new List<planetCost>();
 
-            // send half rounded down of our ships from each planet we do own
+            Console.WriteLine($"Target Planet: {targetPlanet.Id}:{targetPlanet.NumberOfShips}");
             foreach (var planet in gs.Planets.Where(p => p.OwnerId == MyId))
             {
-                var ships = (int)Math.Floor(planet.NumberOfShips / 2.0);
-                if (ships > 0)
+                ownedShips += planet.NumberOfShips;
+            };
+
+            // send half rounded down of our ships from each planet we do own
+            foreach (var planet in gs.Planets.Where(p => (p.OwnerId != MyId) && (p.NumberOfShips < ownedShips)))
+            {
+                int planetValue = (200 - gs.CurrentTurn) * planet.GrowthRate;
+                if (planet.OwnerId != opplayer)
                 {
-                    SendFleet(planet.Id, targetPlanet.Id, ships);
-                }
+                    planetValue -= planet.NumberOfShips;
+                };
+                targetList.Add(new planetCost(planet, planetValue));
             }
+
+            targetList = targetList.OrderBy(o => o.cost).ToList();
+
+            for (int i = targetList.Count - 1; i >= 0; i--)
+            {
+                foreach (var planet in gs.Planets.Where(p => (p.OwnerId == MyId)))
+                {
+                    var minHp = planet.GrowthRate + 1;
+                    int shipsToSend = planet.NumberOfShips - minHp;
+
+                    if (targetList[i].planet.NumberOfShips < planet.NumberOfShips - 1)
+                    {
+                        shipsToSend = targetList[i].planet.NumberOfShips + 1;
+                    }
+
+                    if (planet.NumberOfShips >= minHp)
+                    {
+                        SendFleet(planet.Id, targetList[i].planet.Id, shipsToSend);
+                    }
+
+                };
+            }
+
+
         }
     }
 }
