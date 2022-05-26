201415325d7c6539ef2469213ae9868103176057
Sham <shamir.parmar@lexisnexis.co.uk>
2022-05-26 14:36:58 +0100

    targeting oppenents

--- CSharpAgentShamz/Agent.cs
+++ CSharpAgentShamz/Agent.cs
@@ -26,31 +26,41 @@ namespace CSharpAgent
             var neutralPlanets = gameState.Planets.Where(p => p.OwnerId == -1) ;
             var opponentPlanets = gameState.Planets.Where(p => p.OwnerId != MyId & p.OwnerId != -1);
             var opponentGrowthRate = opponentPlanets.Select(x => x.GrowthRate).Sum();
+            var ourPlanets = gameState.Planets.Where(p => p.OwnerId == MyId);
+            var enemyId = MyId == 1 ? 2 : 1;
 
-    
+            var underAttack = gameState.Fleets.Where(x => x.OwnerId == enemyId && ourPlanets
+                    .Where(y => y.Id == x.DestinationPlanetId).Count() > 0)
+                    .Select(x => x.DestinationPlanetId);
+
             // send half rounded down of our ships from each planet we do own
             foreach (var planet in gameState.Planets.Where(p => p.OwnerId == MyId))
             {
                 var untragetedNeutralPlanets = neutralPlanets.Where(x => !targetPlanets.Contains(x.Id)).ToList();
                 Planet targetPlanet;
+                var ships = (int)Math.Floor(planet.NumberOfShips / 2.0);
+                if (underAttack.Contains(planet.Id))
+                {
+                    ships = (int)Math.Floor(planet.NumberOfShips / 4.0);
+                }
+                else
+                {
+                    ships = (int)Math.Floor(planet.NumberOfShips / 1.25);
+                }
+
                 if(untragetedNeutralPlanets.Count > 0)
                 {
                     targetPlanet =untragetedNeutralPlanets.OrderBy(x => GetPlanetDistance(x, planet)).FirstOrDefault();
                 }
                 else
                 {
-                   targetPlanet  = neutralPlanets.OrderBy(x => GetPlanetDistance(x, planet)).FirstOrDefault();
-                }                
-
-                if (targetPlanet == null)
-                {
-                    targetPlanet = opponentPlanets.OrderBy(x => GetPlanetDistance(x, planet)).FirstOrDefault();
-                    
-                }
+                   targetPlanet  = opponentPlanets.OrderBy(x => GetPlanetDistance(x, planet)).FirstOrDefault();
+                }  
+           
 
                 targetPlanets.Add(targetPlanet.Id);
 
-                var ships = (int)Math.Floor(planet.NumberOfShips / 2.0);
+                
                 if (ships > 0)
                 {
                     SendFleet(planet.Id, targetPlanet.Id, ships);
