a5340e96858831457eb3c2b15254db0a5bcc7609
Peter Charij <CHARIJPX@legal.regn.net>
2022-05-26 14:28:24 +0100

    Refactored project for Carls test agents

--- Agent-EnemyHammer/Agent.cs
+++ Agent-EnemyHammer/Agent.cs
@@ -21,15 +21,15 @@ namespace CSharpAgent
             Console.WriteLine($"Owned Planets: {string.Join(", ", gameState.Planets.Where(p => p.OwnerId == MyId).Select(p =>  p.Id))}");
 
             // find the first planet we don't own
-            var targetPlanet = gameState.Planets.FirstOrDefault(p => p.OwnerId != MyId);
+            var targetPlanet = gameState.Planets.FirstOrDefault(p => p.OwnerId != MyId && p.OwnerId != -1);
             if (targetPlanet == null) return;
 
             Console.WriteLine($"Target Planet: {targetPlanet.Id}:{targetPlanet.NumberOfShips}");                       
 
-            // send half rounded down of our ships from each planet we do own
+            // send available ships from each planet we own
             foreach (var planet in gameState.Planets.Where(p => p.OwnerId == MyId))
             {
-                var ships = (int)Math.Floor(planet.NumberOfShips / 2.0);
+                var ships = planet.NumberOfShips - 1;
                 if (ships > 0)
                 {
                     SendFleet(planet.Id, targetPlanet.Id, ships);
