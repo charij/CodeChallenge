9067b70933883d38db7cb9c2bb78345351e0fdaa
Peter Charij <CHARIJPX@legal.regn.net>
2022-06-17 09:49:03 +0100

    Added template bots

---
+++ PlanetWars.Bot.HammerRandom/Bot.cs
@@ -0,0 +1,70 @@
+﻿namespace PlanetWars.Client
+{
+    using PlanetWars.Common.Comm;
+    using PlanetWars.Common.Data;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    /// <summary>
+    /// AI Bot
+    /// </summary>
+    public class Bot
+    {
+        private readonly Player profile;
+        private readonly Game gameState;
+        private Guid? currentTargetId = null;
+
+        /// <summary>
+        /// Creates an instance of this object
+        /// </summary>
+        /// <param name="profile">The player profile of the logged on client</param>
+        /// <param name="gameState">Contains all relevant information for the game</param>
+        public Bot(Player profile, Game gameState)
+        {
+            this.profile = profile;
+            this.gameState = gameState;
+        }
+
+        /// <summary>
+        /// Current Plan is to send all spare ships from planets we own to a single one that we don't
+        /// </summary>
+        /// <returns>Commands for the turn</returns>
+        public CommandRequest[] CalculateMovesForTurn()
+        {
+            var currentTurn = gameState.History.Count - 1;
+            var currentState = gameState.History[currentTurn];
+            var moves = new List<CommandRequest>();
+
+            var myPlanets = currentState.Planets.Where(p => p.OwnerId == profile.Id);
+            var notMyPlanets = currentState.Planets.Where(p => p.OwnerId != profile.Id);
+
+            var targetPlanet = currentTargetId == null
+                ? notMyPlanets.Skip(new Random().Next(notMyPlanets.Count())).First()
+                : notMyPlanets.First(i => i.Id == currentTargetId);
+            
+            currentTargetId = targetPlanet.Id;
+
+            Console.WriteLine($"[{DateTime.Now.ToShortTimeString()}] Current Turn: {currentTurn}");
+            Console.WriteLine($"Owned Planets: {string.Join(", ", currentState.Planets.Where(p => p.OwnerId == profile.Id).Select(p => p.Id))}");
+            Console.WriteLine($"Target Planet: {targetPlanet.Id}:{targetPlanet.ShipCount}");
+
+            // send available ships from each planet we own
+            foreach (var planet in currentState.Planets.Where(p => p.OwnerId == profile.Id))
+            {
+                var ships = planet.ShipCount - 1;
+                if (ships > 0)
+                {
+                    moves.Add(new CommandRequest
+                    {
+                        SourcePlanetId = planet.Id,
+                        TargetPlanetId = targetPlanet.Id,
+                        ShipCount = ships,
+                    });
+                }
+            }
+
+            return moves.ToArray();
+        }
+    }
+}
