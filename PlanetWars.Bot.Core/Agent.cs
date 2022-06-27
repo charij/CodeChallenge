34acec4c854be805ab3742554e5c1b3aabd8c776
Andy Lewis <andylewis9@googlemail.com>
2022-06-27 09:49:49 +0100

    Added dot net core bot for F# support with sample bot (#1)
    Co-authored-by: Andy Lewis <andy.lewis@lexisnexix.co.uk>

---
+++ PlanetWars.Bot.Core/Agent.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PlanetWars.Shared;
+using PlanetWars.Bot.FSharp;
+
+namespace CSharpAgentCore
+{
+    public class Agent : AgentBase
+    {
+        public Agent(string name, string endpoint) : base(name, endpoint){}
+
+        /// <summary>
+        /// Do your cool AI stuff
+        /// </summary>
+        /// <param name="gameState"></param>
+        public override void Update(StatusResult gameState)
+        {
+            var moves = AgentFSharp.processGameState(MyId, gameState);
+
+            foreach (var move in moves )
+            {
+                Console.WriteLine($"\n Target Planet: {move.TargetPlanetId} : ships {move.NumberOfShips}");
+                SendFleet(move.SourcePlanetId, move.TargetPlanetId, move.NumberOfShips);
+            }
+        }
+    }
+}
---
+++ PlanetWars.Bot.FSharp/AgentFSharp.fs
@@ -0,0 +1,63 @@
+﻿namespace PlanetWars.Bot.FSharp
+
+module AgentFSharp =
+    open PlanetWars.Shared
+    open System
+
+    type FleetMove = {
+        SourcePlanetId : int
+        TargetPlanetId : int
+        NumberOfShips : int
+    }
+
+    let logPLanets title planets =
+        printf "%s %s: " Environment.NewLine title
+        planets |> List.iter (fun (p:Planet) -> printf " %i " p.Id )
+
+    let processGameState myId (gameState:StatusResult) : FleetMove list =
+        let enemyPlanets = 
+            gameState.Planets 
+            |> Seq.toList 
+            |> List.filter (fun (p:Planet) -> p.OwnerId <> myId) 
+        
+        let ownedPlanets = 
+            gameState.Planets 
+            |> Seq.toList 
+            |> List.filter(fun (p:Planet) -> p.OwnerId = myId)     
+        
+        ownedPlanets |> logPLanets "Owned Planets"
+
+        let createFleetMove (planet:Planet) =
+            let closestPlanets planets =
+                planets
+                |> List.sortBy (fun (p:Planet) -> planet.Position.Distance(p.Position))
+       
+            let firstAttackablePlanet targetPlanets =
+                targetPlanets
+                |> List.tryFind (fun (p:Planet) -> planet.NumberOfShips > p.NumberOfShips )
+
+            let createMove targetPlanet =
+                match targetPlanet with
+                | Some (p:Planet) -> Some {SourcePlanetId = planet.Id; TargetPlanetId = p.Id; NumberOfShips = p.NumberOfShips + 1}
+                | None -> None
+        
+            enemyPlanets
+            |> closestPlanets
+            |> firstAttackablePlanet 
+            |> createMove
+
+        let rec generateMoves fleetMoves owned =
+            match owned with
+            | currentPlanet::remainingOwnedPlanets -> 
+                match currentPlanet |> createFleetMove with
+                | Some newMove -> remainingOwnedPlanets |> generateMoves (newMove::fleetMoves)
+                | None -> remainingOwnedPlanets |> generateMoves fleetMoves
+            | [] -> fleetMoves
+
+        ownedPlanets
+        |> generateMoves List.empty
+
+
+
+
+
